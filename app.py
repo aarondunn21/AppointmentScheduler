c70f88c968fb3370b86efd5d50e3069239ab4b7e
Aaron Dunn <2000ad21@gmail.com>
2023-02-17 01:16:54 -0500

    List users and their delete buttons

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request
 from extensions import mongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb+srv://admin:Password123@appointmentscheduler.ikvbn6u.mongodb.net/mydb?retryWrites=true&w=majority'
@@ -8,7 +9,9 @@ mongo.init_app(app)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    user_collection = mongo.db.Users
+    users_list = user_collection.find()
+    return render_template('index.html', users=users_list)
 
 @app.route("/add_user", methods =['POST'])
 def add_user():
@@ -19,6 +22,14 @@ def add_user():
     user_collection.insert_one({'text' : user_item})
     # add name into table
     return redirect(url_for('home'))
+
+@app.route("/delete_user/<id>")
+def delete_user(id):
+    print(id)
+    user_collection = mongo.db.Users
+    my_query = {'_id' : ObjectId(id)}
+    user_collection.delete_one(my_query)
+    return redirect(url_for('home'))
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="localhost", port=5000)
